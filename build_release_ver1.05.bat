40f56a45725dc61287d7dc1c1f2917bc7d7f6ec0
あまおう <58240927+Amaou0014@users.noreply.github.com>
2025-08-19 17:05:41 +0900

    1.05のブランチ作成。動画背景色をメニューから設定できるように修正。

--- build_release_ver1.05.bat
+++ build_release_ver1.05.bat
@@ -6,13 +6,13 @@ rmdir /S /Q dist
 rmdir /S /Q build
 rmdir /S /Q __pycache__
 
-REM === VigStreamClip を build/release/VigStreamClip_ver1.04 にビルド ===
+REM === VigStreamClip を build/release/VigStreamClip_ver1.05 にビルド ===
 
 echo pyinstaller最新版をインストールします...
 pip install --upgrade pyinstaller
 
 REM 設定
-SET VER=ver1.04
+SET VER=ver1.05
 SET NAME=VigStreamClip
 SET DIST=build\release\%NAME%_%VER%
 SET BUILD=build\release
